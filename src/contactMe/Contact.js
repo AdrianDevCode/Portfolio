02d26681fae93b5d9f1bf34d230b1c6bc2c25c0b
Adrian Wiszniewski <Adrian.Wiszniewski@ricoh-usa.com>
2018-07-11 15:59:07 -0400

    added google maps and styled

--- src/contactMe/Contact.js
+++ src/contactMe/Contact.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import './Contact.css';
 
+
 class Contact extends Component {
     render() {
         return (
             <section id="contact">
                 <div className="container-fluid">
                     <div className="row">
-                        <div className="col-md-12 d-sm-inline-flex text-center">
-                            <div id="map"></div>
-                            <div id="form" action="https://formspree.io/adrianw.ct@gmail.com" method="POST" role="form">
+                        <div className="col-md-12  text-center">
+                            <div className="ml-auto" id="form" action="https://formspree.io/adrianw.ct@gmail.com" method="POST" role="form">
                                 <h3>Send me a message!</h3>
                                 <div className="row">
                                     <div className="col-md-12">
                                         <div className="form-group">
-                                        <textarea id="form_message" name="message" className="form-control" rows="4" required="required" data-error="Oops, need something in message"></textarea>
+                                        <textarea id="form_message" name="message" className="form-control" rows="7" required="required" data-error="Oops, need something in message"></textarea>
                                         <div className="help-block with-errors"></div>
                                         </div>
                                     </div>
@@ -25,10 +25,6 @@ class Contact extends Component {
                             </div>
                         </div>
                     </div>
-                    <footer>
-                        <strong>
-                        <i className="fa fa-copyright" aria-hidden="true"></i> copyright 2017</strong>
-                    </footer>
                 </div>
             </section>
 
---
+++ src/contactMe/GoogleMap.js
@@ -0,0 +1,250 @@
+import React, {Component} from 'react';
+import {Map, GoogleApiWrapper, Marker} from 'google-maps-react';
+import './Contact.css';
+
+export class MapContainer extends Component {
+  render() {
+    const style = {
+        width: '100%',
+        Height: '100vh',  
+      }
+    const {props} = this,
+        {mapStyles} = props;
+    return (
+      
+      <Map
+        google={this.props.google}
+        style={style}
+        styles={mapStyles}
+        initialCenter={{
+        lat: 33.8900,
+        lng: -84.2144
+      }}
+        zoom={10}>
+        <Marker
+            title={'I live here'}
+            name={'Smyrna'}
+            position={{lat: 33.8900, lng: -84.5144}} />
+      
+      </Map>
+     
+    );
+  }
+}
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyA_0VyBKrqsTY0gwf05J8It5nuEmEIdmjA')
+})(MapContainer)
+
+MapContainer.defaultProps = {
+mapStyles: [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#181818"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1b1b1b"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#2c2c2c"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8a8a8a"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#373737"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#3c3c3c"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#4e4e4e"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#000000"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#3d3d3d"
+      }
+    ]
+  }
+]
+}
